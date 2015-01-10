16b3ffc3f487f09816015df890c37b8769c0cfc1
anycmd <anycmd@qq.com>
2015-01-10 10:02:14 +0800

    Repl

--- src/Examples/Anycmd.Repl/Program.cs
+++ src/Examples/Anycmd.Repl/Program.cs
@@ -14,7 +14,6 @@ namespace Anycmd.Repl
 	using System.Diagnostics;
 	using System.IO;
 	using System.Reflection;
-	using System.Security.Principal;
 
 	class Program
 	{
@@ -25,46 +24,22 @@ namespace Anycmd.Repl
 			//程序没有运行
 			try
 			{
-				// 当前用户是管理员的时候，直接启动应用程序，如果不是管理员，
-				// 则使用启动对象启动程序，以确保使用管理员身份运行
-				var identity = WindowsIdentity.GetCurrent();
-				Debug.Assert(identity != null, "identity != null");
-				var principal = new WindowsPrincipal(identity);
-				if (principal.IsInRole(WindowsBuiltInRole.Administrator))
-				{
-					EfContext.InitStorage(new SimpleEfContextStorage());
-					// 环境初始化
-					var acDomain = new DefaultAcDomain();
-					_acDomain = acDomain;
-					_acDomain.AddService(typeof(ILoggingService), new Log4NetLoggingService(_acDomain));
-					_acDomain.AddService(typeof(IUserSessionStorage), new SimpleUserSessionStorage());
-					acDomain.Init();
-					_acDomain.RegisterRepository(new List<string>
-					{
+				EfContext.InitStorage(new SimpleEfContextStorage());
+				// 环境初始化
+				var acDomain = new DefaultAcDomain();
+				_acDomain = acDomain;
+				_acDomain.AddService(typeof(ILoggingService), new Log4NetLoggingService(_acDomain));
+				_acDomain.AddService(typeof(IUserSessionStorage), new SimpleUserSessionStorage());
+				acDomain.Init();
+				_acDomain.RegisterRepository(new List<string> {
 						"EdiEntities",
 						"AcEntities",
 						"InfraEntities",
 						"IdentityEntities"
 					}, typeof(AcDomain).Assembly);
-					_acDomain.RegisterEdiCore();
+				_acDomain.RegisterEdiCore();
 
-					Run(args);
-				}
-				else
-				{
-					//创建启动对象
-					var startInfo = new ProcessStartInfo
-					{
-						FileName = Assembly.GetExecutingAssembly().Location,
-						Arguments = String.Join(" ", args),
-						Verb = "runas"
-					};
-					//设置运行文件
-					//设置启动参数
-					//设置启动动作,确保以管理员身份运行
-					//如果不是管理员，则启动UAC
-					Process.Start(startInfo);
-				}
+				Run(args);
 			}
 			catch (Exception ex)
 			{
